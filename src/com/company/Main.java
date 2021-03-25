03dcf9fcf1ee9685bf1d10d7ed46c9f13e127af9
acuering <cueringalvis@gmail.com>
2021-03-25 06:38:50 -0700

    firstcommit

---
+++ src/com/company/Main.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        //Primitive
+        int wholeNumbers = 122;
+        long LargeDecimalNumbers = -12;
+        byte VariableRange = 123;
+        short mediumRangeDecimalVariable = 32767;
+        float numberWithDecimalPlaces = 8.3f;
+        double largeNumberWithDecimalPlaces = 321321.3223;
+        boolean check = true;
+        char singleCharacter = '\u2511';
+
+        // print all primitive types, output and run
+        System.out.print(wholeNumbers);
+        System.out.print(LargeDecimalNumbers);
+        System.out.print(VariableRange);
+        System.out.print(mediumRangeDecimalVariable);
+        System.out.print(numberWithDecimalPlaces);
+        System.out.print(largeNumberWithDecimalPlaces);
+        System.out.println(check);
+        System.out.print(singleCharacter);
+
+        double doubleNumber;
+        int i = 23;
+        doubleNumber = i;
+
+
+        //Non Primitive
+    }
+}
