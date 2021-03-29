0ac5cb5d2b88b0237dedc7503d5e969491821ab0
acuering <cueringalvis@gmail.com>
2021-03-29 04:25:24 -0700

    operatos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,8 +27,105 @@ public class Main {
         double doubleNumber;
         int i = 23;
         doubleNumber = i;
+            System.out.print(doubleNumber);
+
+            //switch case
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
 
 
-        //Non Primitive
+        //for loops
+
+        for (int x = 0; x < 10; x++) {
+            System.out.print(x);
+        }
+        //Arithmetic Operators
+
+        int x = 10;
+        int y = 20;
+        int result = x + y;
+        System.out.println(result);
+
+        int a, b;
+        a = 10;
+        b = (a == 1) ? 20: 30;
+        System.out.println( "Value of b is : " +  b );
+
+        b = (a != 10) ? 20: 30;
+        System.out.println( "Value of b is : " + b );
+
+        a = 5;
+        b = 2;
+        if (a > b) {
+
+            System.out.println("x is greater than y");
+        }
+
+        a = 5;
+        b = 2;
+        if (a < b) {
+
+            System.out.println("x is greater than y");
+        }
+
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+
+        // COMPOUND ASSIGNMENT OPERATORS
+        int number = 5;
+        number    +=  3;
+        System.out.println(number);
+        number    -=  3;
+        System.out.println(number);
+        number    *=  3;
+        System.out.println(number);
+        number    /=  3;
+        System.out.println(number);
+        number    %=  3;
+        System.out.println(number);
+
+        //INCREMENT AND DECREMENT OPERATORS
+        int banana = 10;
+        banana++;
+        System.out.println(banana);
+
+        int bananas= 20;
+        bananas--;
+        System.out.println(bananas);
+
+
+        int incrementOp1 = 5;
+        int incrementOp2 = 5;
+
+        int incrementOp3 = incrementOp1 * incrementOp2++;
+        System.out.println(incrementOp3);
+        int incrementOp4 = incrementOp1 * --incrementOp2;
+        System.out.println(incrementOp4);
     }
 }
